08c8b2f239f983ccaa0e5c39062f097fafdf4f0a
Somkiet <somsor@perfenterprise.com>
2018-09-22 12:28:58 +0700

    test1

---
+++ TestLoadG5/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestLoadG5.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
