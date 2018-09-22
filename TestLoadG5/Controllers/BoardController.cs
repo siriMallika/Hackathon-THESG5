14c2776a9e6be369e59ac777911ce858ea96c1b1
Somkiet <somsor@perfenterprise.com>
2018-09-22 13:59:29 +0700

    add board controller

---
+++ TestLoadG5/Controllers/BoardController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestLoadG5.Models;
+
+namespace TestLoadG5.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BoardController : Controller
+    {
+        public static List<Board> Boards = new List<Board>();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Board>> List()
+        {
+            return Boards;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Board> Get(string id)
+        {
+            return Boards.FirstOrDefault(x => x.Id == id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody]Board request)
+        {
+            request.Id = Guid.NewGuid().ToString();
+            Boards.Add(request);
+        }
+
+        [HttpPost("{id}")]
+        public void Edit(string id, [FromBody]Board request)
+        {
+            var product = Boards.FirstOrDefault(x => x.Id == id);
+            product.Title = request.Title;
+            product.Detail = request.Detail;
+        }
+
+        [HttpPost("{id}")]
+        public void Delete(string id)
+        {
+            Boards = Boards.Where(x => x.Id != id).ToList();
+        }
+
+        [HttpGet]
+        public void UseMockProducts()
+        {
+            Boards = new List<Board>{
+                new Board{
+                    Id = Guid.NewGuid().ToString(),
+                    Title = "LG TV",
+                    Detail = "LG1234",
+                },
+                new Board{
+                    Id = Guid.NewGuid().ToString(),
+                    Title = "iPhone Z",
+                    Detail = "PZ3452",
+                },
+                new Board{
+                    Id = Guid.NewGuid().ToString(),
+                    Title = "mi Pro 5",
+                    Detail = "PRO258",
+                },
+                new Board{
+                    Id = Guid.NewGuid().ToString(),
+                    Title = "V9",
+                    Detail = "V93654",
+                },
+            };
+        }
+
+        [HttpGet]
+        public void Reset()
+        {
+            Boards = new List<Board>();
+        }
+    }
+}
---
+++ TestLoadG5/Models/Board.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestLoadG5.Models
+{
+    public class Board
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Detail { get; set; }
+        public IEnumerable<Post> Posts { get; set; }
+    }
+
+    public class Post
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string Title { get; set; }
+        public string Detail { get; set; }
+    }
+}
